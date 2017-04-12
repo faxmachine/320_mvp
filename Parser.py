e1bc9619fce4bfae993290afd0bc22c5735fc516
Benjamin Carroll <b3ncarroll@gmail.com>
2017-04-12 12:58:26 -0400

    added test data, began parser

---
+++ Parser.py
@@ -0,0 +1,7 @@
+# consider pydub for getting samplerate
+
+class Parser:
+	file = "test_data/MindsEye_-_This_or_That.mp3"
+	def loadFile():
+	
+	def average(i, j):
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # 320_mvp
 Music Visualizer for CS 320
+
+test data info:
+license: https://creativecommons.org/licenses/by-sa/4.0/
+song: This or That by MindsEye
