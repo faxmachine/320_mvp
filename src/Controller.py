50def0fd012b5a80bd9235cf3bf2c2aaa3bdcedb
Jakedjoe <Jakedjoe@gmail.com>
2017-04-25 20:44:13 -0400

    added style2

--- src/Controller.py
+++ src/Controller.py
@@ -1,5 +1,6 @@
 import Parser as p
-import FrameGenerator as f
+import FrameGenerator1 as f1
+import FrameGenerator2 as f2
 import Combine as c
 
 
@@ -7,18 +8,32 @@ musicFilePath = "../test_data/MindsEye_-_This_or_That.mp3"
 framesPerSecond = 29
 m = 3
 
-# def Go(musicFilePath, style, ):
+def go(musicFilePath, style):
 
-# Parses the mp3 file and extracts features
-features = p.getSamples(musicFilePath, m)
+	# Parses the mp3 file and extracts features
+	features = p.getSamples(musicFilePath, m)
 
-# Generates frames from output of parser
-frames = f.GenerateAllFrames(features)
+	# Style selection
+	# Generates frames from output of parser
+	frames = []
 
-# Combines mp3 and frames into a video output
-success = c.createMP4(musicFilePath, frames, framesPerSecond)
+	# Generates frames with style 1
+	print ("Generating Frames")
 
+	if style == 1:
+		frames = f1.GenerateAllFrames(features)
+	if style == 2:
+		frames = f2.GenerateAllFrames(features)
 
+	print ("Combining Frames With Music")
+	
+	# Combines mp3 and frames into a video output
+	success = c.createMP4(musicFilePath, frames, framesPerSecond)
+
+	if success is True:
+		print ("Video Successfully Created")
+
+go(musicFilePath, 2)
 
 
 
